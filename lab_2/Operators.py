bdcd03084f1aefde4e5e080f78b310515114c5b6
Eldiyar <el.talgatow@gmail.com>
2024-01-25 16:47:34 +0600

    Operators

---
+++ lab_2/Operators.py
@@ -0,0 +1,18 @@
+print(10*5)
+#50
+
+print (10/2)
+#5
+
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+#Yes, apple is a fruit!
+    
+if 5 != 10:
+    print("5 and 10 is not equal")
+#5 and 10 is not equal
+    
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
+#At least one of the statements is true
--- lab_2/Operators_ex5.py
+++
@@ -1,2 +0,0 @@
-if 5 == 10 or 5 == 5:
-    print("At least one of the statements is true")
