e8ea5808c8315817c1f24cb11365ec2034d99ae1
Eldiyar <el.talgatow@gmail.com>
2024-01-25 17:26:02 +0600

    Tuples

---
+++ lab_2/Tuples.py
@@ -0,0 +1,15 @@
+fruits = ("apple", "banana", "cherry")
+print(fruits[0])
+#apple
+
+fruits = ("apple", "banana", "cherry")
+print(len(fruits))
+#3
+
+fruits = ("apple", "banana", "cherry")
+print(fruits[-1])
+#cherry
+
+fruits = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(fruits[2:5])
+#cherry, orange, kiwi
