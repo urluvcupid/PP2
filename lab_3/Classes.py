941d826ffca9ab5284931f82779059e286f78f8d
Eldiyar <el.talgatow@gmail.com>
2024-02-01 17:20:39 +0600

    Classes_partFromW3s

---
+++ lab_3/Classes.py
@@ -0,0 +1,21 @@
+#1
+class MyClass:
+    x = 5
+
+#2
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+#3
+class MyClass:
+    x = 5
+p1 = MyClass()
+
+print(p1.x)    #5
+
+#4
+class Person:
+  def __init__ (self, name, age):
+    self.name = name
+    self.age = age
