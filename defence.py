4e98eb4eb15de959ded3727bfc170594475d4e76
Eldiyar <el.talgatow@gmail.com>
2024-09-06 20:06:02 +0500

    12

--- defence.py
+++ defence.py
@@ -1,26 +1,11 @@
-import pygame 
-import random
+a = int(2)
+b = complex(a)
+res = int(b)
 
-pygame.init()
+c = float(3.14)
+d = complex(c)
+res1 = float(d)
 
-screen = pygame.display.set_mode((1000, 555))
-screen.fill((255, 255, 255))
 
-done = False
-
-pygame.display.set_caption("something")
-
-block_list = [pygame.Rect(650 + 120 * i, 250 + 70 * j,
-                          100, 50) for i in range(3) for j in range(4)]
-color_list = [(random.randrange(0, 255),
-               random.randrange(0, 255), random.randrange(0, 255))
-              for i in range(3) for j in range(4)]
-
-for i, block in enumerate(block_list):
-    pygame.draw.rect(screen, color_list[i], block)
-
-while not done:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-    pygame.display.flip()
+print(res, res1)
+#adad
